d79a121723e6095f1f80e4182ca66e66093a07e8
Salsha Armenia Amosea <36811915+salshaamosea@users.noreply.github.com>
2020-02-17 23:28:59 +0700

    Add files via upload

---
+++ Tugas 1/client.py
@@ -0,0 +1,28 @@
+import sys
+import socket
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('127.0.0.1', 30003)
+#print(f"connecting to {server_address} port {server_address}")
+#print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+
+try:
+    # Send data
+    message = 'PEMROGRAMAN JARINGAN TEKNIK INFORMATIKA'
+    print(f"sending {message}")
+    #print >>sys.stderr, 'sending "%s"' % message
+    sock.sendall(message.encode())
+    # Look for the respon)se
+    amount_received = 0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = sock.recv(64)
+        amount_received += len(data)
+        print(f"{data}")
+finally:
+    print(f"closing")
+    sock.close()
