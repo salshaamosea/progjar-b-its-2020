f83c5461ab050d4569a034f12a60b1a11243972f
Salsha Armenia Amosea <36811915+salshaamosea@users.noreply.github.com>
2020-02-17 23:55:42 +0700

    Update server3.py

--- Tugas 1/server3.py
+++ Tugas 1/server3.py
@@ -3,7 +3,7 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('127.0.0.1', 30003)
+server_address = ('127.0.0.1', 31002)
 print(f"starting up on {server_address}")
 sock.bind(server_address)
 
@@ -26,31 +26,3 @@ while True:
             break
     #clean up connection
     connection.close()
-
-# import sys
-# import socket
-# # Create a TCP/IP socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # Bind the socket to the port
-# server_address = ('localhost', 10000)
-# print >>sys.stderr, 'starting up on %s port %s' % server_address
-# sock.bind(server_address)
-# # Listen for incoming connections
-# sock.listen(1)
-# while True:
-#     	# Wait for a connection
-#     	print(sys.stderr, 'waiting for a connection')
-#     	connection, client_address = sock.accept()
-#     	print >>sys.stderr, 'connection from', client_address
-#     	# Receive the data in small chunks and retransmit it
-#     	while True:
-#         	data = connection.recv(32)
-#         	print >>sys.stderr, 'received "%s"' % data
-#             	if data:
-#                 	print >>sys.stderr, 'sending data back to the client'
-#                 	connection.sendall('-->'+data)
-#             	else:
-#                 	print >>sys.stderr, 'no more data from', client_address
-#                 	break
-#         # Clean up the connection
-# 	connection.close()
