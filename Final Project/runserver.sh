8690d2bcced5ae5d3ae96e3c92c8b961ac33c75c
Salsha Armenia Amosea <36811915+salshaamosea@users.noreply.github.com>
2020-05-22 23:39:23 +0700

    Create runserver.sh

---
+++ Final Project/runserver.sh
@@ -0,0 +1,7 @@
+#jalankan 5 async_server
+
+python3 async_server.py 9002 &
+python3 async_server.py 9003 &
+python3 async_server.py 9004 &
+python3 async_server.py 9005 &
+python3 async_server.py 9006
